2df657a56f8b25bc9e8b1526b948495ae381cc1a
Tanmay175 <sahatanmay108@gmail.com>
2026-09-07 22:07:49 +0530

    home page ui is designed

--- FRONTEND/src/App.routes.jsx
+++ FRONTEND/src/App.routes.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from "react-router";
 import Login from './features/auth/pages/Login.jsx'
 import Register from './features/auth/pages/Register.jsx'
 import Protect from "./features/auth/components/protect.jsx";
+import Home from "./features/Interview/pages/Home.jsx";
 
 
 const router=createBrowserRouter([
@@ -15,7 +16,7 @@ const router=createBrowserRouter([
     },
     {
         path:"/",
-        element:<Protect><h1>home page</h1></Protect>
+        element:<Protect><Home/></Protect>
     }
 ])
 
