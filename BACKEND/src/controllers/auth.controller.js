3cd1d1e9db4734cac570ffdad631eda2e47a93c8
Tanmay175 <sahatanmay108@gmail.com>
2026-09-12 20:52:58 +0530

    fix production authentication cookies

--- BACKEND/src/controllers/auth.controller.js
+++ BACKEND/src/controllers/auth.controller.js
@@ -37,7 +37,12 @@ async function registerUserController(req,res){
         {expiresIn:"1d"}
     )
 
-    res.cookie("token",token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 24 * 60 * 60 * 1000
+})
 
     res.status(201).json({
         message:"user created successfully",
@@ -75,7 +80,12 @@ async function loginUserController(req,res){
         {expiresIn:"1d"}
     )
 
-    res.cookie("token",token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 24 * 60 * 60 * 1000
+})
 
     res.status(200).json({
         message:"Login successful",
@@ -93,7 +103,11 @@ async function logoutUserController(req,res){
     if (token){
         await blacklistModel.create({token})
     }
-    res.clearCookie("token")
+    res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
+})
     res.status(200).json({
         message:"Logout successful"
     })
