158f188849336dc986709ebf3b3107a98bf74e86
Tanmay175 <sahatanmay108@gmail.com>
2026-09-12 13:34:30 +0530

    fix deployment bugs

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: proceess.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL,
     credentials: true
 }))
 
